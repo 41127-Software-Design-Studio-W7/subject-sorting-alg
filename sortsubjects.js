06b4e6230e87d18f85229c1464bfce287b59ae44
Alex Trobec <alex.trobec@immutable.com>
2022-09-14 19:42:01 +1000

    Comparison function for bespoke Subject objects

--- sortsubjects.js
+++ sortsubjects.js
@@ -1,4 +1,21 @@
 function CompareSubjects(s, t) {
+    /*
+    Returns true if s is alphabetically "less than / before" t. Returns false otherwise.
+    */
+    for (let i = 0 ; i < s.length && i < t.length ; i++) {
+        if (i < s.length && i < t.length) {
+            if (s[i].score > t[i].score) {
+                return true;
+            }
+        }
+        if (i >= s.length) {
+            return true;
+        } 
+        else {
+            return false;
+        }
+        
+    }
     return true;
 }
 
